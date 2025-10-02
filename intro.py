db916ded1e64577e37a0d50d466beca0be79d1b2
Humoyun Valijonov <v.humoyun.b@gmail.com>
2025-10-02 15:30:35 +0500

    initial commit : Create interactive introduction programm

---
+++ intro.py
@@ -0,0 +1,4 @@
+Full_name = input('What is your name') 
+Major = input('What is your major')
+Hobby = input ('What is your favourite hobby')
+print(f"Your name is {Full_name}, Your major is {Major}, Your favourite hobby is {Hobby}")

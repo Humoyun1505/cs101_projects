075cc04dbb89c54fcd4150bff4e2a836baad3b22
Humoyun Valijonov <v.humoyun.b@gmail.com>
2025-10-07 13:03:16 +0500

    Temperature converter

---
+++ exercise_1.py
@@ -0,0 +1,7 @@
+Celsius = float(input('Temperature of the weather in celsium:'))
+
+fahrenheit = Celsius * 9/5 + 32
+
+Kelvin = Celsius + 273.15
+
+print (f"Temperature of the weather in celsius: {Celsius} , \n Temperature of the weather in fahrenheit: {fahrenheit} , \n Temperature of the weather in Kelvin: {Kelvin} ")
